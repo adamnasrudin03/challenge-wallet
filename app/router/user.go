44047e73bce9b997b0355d27a1090c253877308b
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-15 14:44:46 +0700

    route user

---
+++ app/router/user.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"adamnasrudin03/challenge-wallet/app/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRouter(e *gin.Engine, userController controller.UserController) {
+	userRoutes := e.Group("/api/v1/auth")
+	{
+		userRoutes.POST("/register", userController.Register)
+		userRoutes.POST("/login", userController.Login)
+	}
+}

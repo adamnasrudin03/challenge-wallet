672d488e149db253fb583ac742a2218f09ecdbfe
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-15 14:44:17 +0700

    wire user

---
+++ app/wire.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"adamnasrudin03/challenge-wallet/app/repository"
+	"adamnasrudin03/challenge-wallet/app/service"
+
+	"gorm.io/gorm"
+)
+
+func WiringRepository(db *gorm.DB) *repository.Repositories {
+	return &repository.Repositories{
+		User: repository.NewUserRepository(db),
+	}
+}
+
+func WiringService(repo *repository.Repositories) *service.Services {
+	return &service.Services{
+		User: service.NewUserService(repo.User),
+	}
+}

3d8b431f202da3e986bc807dbe21f0118466ecc6
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-16 10:17:58 +0700

    route users

--- app/router/user.go
+++ app/router/user.go
@@ -2,14 +2,21 @@ package router
 
 import (
 	"adamnasrudin03/challenge-wallet/app/controller"
+	"adamnasrudin03/challenge-wallet/app/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRouter(e *gin.Engine, userController controller.UserController) {
-	userRoutes := e.Group("/api/v1/auth")
+	authRoutes := e.Group("/api/v1/auth")
 	{
-		userRoutes.POST("/register", userController.Register)
-		userRoutes.POST("/login", userController.Login)
+		authRoutes.POST("/register", userController.Register)
+		authRoutes.POST("/login", userController.Login)
+	}
+
+	userRoutes := e.Group("/api/v1/users")
+	{
+		userRoutes.Use(middlewares.Authentication())
+		userRoutes.GET("/my-wallet", middlewares.CheckAuthorization(), userController.MyWallet)
 	}
 }

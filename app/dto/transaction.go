996d7905a34feb3b02e135ba94be8cb3eb8c6e2a
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-16 10:48:49 +0700

    fix validation

--- app/dto/transaction.go
+++ app/dto/transaction.go
@@ -1,12 +1,12 @@
 package dto
 
 type CreateTxReq struct {
-	Name     string `json:"name" validate:"required,min=6"`
+	Name     string `json:"name" validate:"required,min=2"`
 	Amount   uint64 `json:"amount"`
 	Quantity uint64 `json:"quantity" validate:"required,min=1"`
 }
 
 type TopUpReq struct {
-	BankName string `json:"bank_name" validate:"required,min=6"`
+	BankName string `json:"bank_name" validate:"required,min=2"`
 	Amount   uint64 `json:"amount"  validate:"required,min=1000"`
 }

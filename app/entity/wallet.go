3c9368f1bdc4759b4e8b5493cef8f56c376a65da
Adam Nasrudin <adamnasrudin858@gmail.com>
2023-04-16 10:06:01 +0700

    adjust entity wallet

--- app/entity/wallet.go
+++ app/entity/wallet.go
@@ -6,6 +6,7 @@ type Wallet struct {
 	ID        uint64    `gorm:"primaryKey" json:"id"`
 	Amount    uint64    `json:"amount" `
 	UserID    uint64    `gorm:"primaryKey" json:"user_id"`
+	User      User      `json:"user,omitempty"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
